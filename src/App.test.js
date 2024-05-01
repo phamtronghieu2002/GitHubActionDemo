8a57baf25b7555e4587b71dd8f91167f2b6a6a76
phamhieu <phamtronghieu2002iuh@gmail.com>
2024-05-01 14:50:46 +0700

    add base url

--- src/App.test.js
+++ src/App.test.js
@@ -4,22 +4,25 @@ import { Authprovider } from "./providers/Authprovider";
 import { Chatprovider } from "./providers/Chatprovider";
 import { Inputprovider } from "./providers/Inputprovider";
 import { ResponsiveProvider } from "./providers/Responsiveprovider";
+import { BrowserRouter as Router } from "react-router-dom";
 import GlobalStyles from "./components/GlobalStyles";
 
 test("renders learn react link", () => {
   render(
   
-    <Authprovider>
-      <Chatprovider>
-        <Inputprovider>
-          <ResponsiveProvider>
-            <GlobalStyles>
+<Authprovider>
+    <Chatprovider>
+      <Inputprovider>
+        <ResponsiveProvider>
+          <GlobalStyles>
+            <Router basename={process.env.PUBLIC_URL}>
               <App />
-            </GlobalStyles>
-          </ResponsiveProvider>
-        </Inputprovider>
-      </Chatprovider>
-    </Authprovider>
+            </Router>
+          </GlobalStyles>
+        </ResponsiveProvider>
+      </Inputprovider>
+    </Chatprovider>
+  </Authprovider>
   );
 
 });
