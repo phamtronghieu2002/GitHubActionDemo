a1247cbf7835af1980272d0bee7a07ac42023a1f
Nguyen Chi Cuong <20020331.cuong@student.iuh.edu.vn>
2024-05-20 18:27:42 +0700

    change text

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -186,7 +186,7 @@ function Form({ login }) {
         </div>
         {login && (
           <p style={{ textAlign: "left", padding: "0 20px" }}>
-            Bạn chưa có tài khoản ?{" "}
+            Bạn chưa có tài khoản phải không ?{" "}
             <Link style={{ margin: "0  0 0 10px" }} to="/register">
               Đăng kí
             </Link>
