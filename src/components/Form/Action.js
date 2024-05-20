27d6a6a785d33b713e4fa34bb79846c55eaa27b0
Nguyen Chi Cuong <20020331.cuong@student.iuh.edu.vn>
2024-05-20 14:48:46 +0700

    add Action.js

---
+++ src/components/Form/Action.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>This is a simple React component.</p>
+        </div>
+    );
+};
+export default Hello;
